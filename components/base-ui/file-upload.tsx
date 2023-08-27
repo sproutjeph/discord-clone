06e452d4683eaad78fa1a0eeb9211e1abd07fed9
jephthah <jephthah.mbah@outlook.com>
2023-08-27 22:01:04 +0100

    added file upload

---
+++ components/base-ui/file-upload.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { FC } from "react";
+import { UploadDropzone } from "@/lib/uploadthing";
+import "@uploadthing/react/styles.css";
+import Image from "next/image";
+import { FileIcon, X } from "lucide-react";
+
+interface fileUploadProps {
+  endpoint: "messageFile" | "serverImage";
+  value: string;
+  onChange: (url?: string) => void;
+}
+
+const FileUpload: FC<fileUploadProps> = ({ endpoint, value, onChange }) => {
+  const fileType = value?.split(".").pop();
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative w-20 h-20">
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          onClick={() => onChange("")}
+          className="absolute top-0 right-0 p-1 text-white rounded-full shadow-sm bg-rose-500"
+          type="button"
+        >
+          <X className="w-4 h-4" />
+        </button>
+      </div>
+    );
+  }
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="w-10 h-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className="absolute p-1 text-white rounded-full shadow-sm bg-rose-500 -top-2 -right-2"
+          type="button"
+        >
+          <X className="w-4 h-4" />
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
+    />
+  );
+};
+
+export default FileUpload;
---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,132 @@
+"use client";
+
+import { FC, useEffect, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import FileUpload from "../base-ui/file-upload";
+
+interface initialModalProps {}
+
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Server Name is Required",
+  }),
+
+  imageUrl: z.string().min(1, {
+    message: "Server Image is Required",
+  }),
+});
+
+const InitialModal: FC<initialModalProps> = ({}) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
+
+  const isLoading = form.formState.isLoading;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <Dialog open>
+      <DialogContent className="p-0 overflow-hidden text-black bg-white">
+        <DialogHeader className="px-6 pt-8">
+          <DialogTitle className="text-2xl font-bold text-center">
+            Customize your server
+          </DialogTitle>
+
+          <DialogDescription className="text-center text-zinc-500">
+            Give your server a personality with a name and an image. You can
+            always change it later
+          </DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="px-8 space-y-8">
+              <div className="flex items-center justify-center text-center">
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="uppercase, text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                      Server Name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        className="text-black border-0 bg-zinc-300/50 focus-visible:ring-0 focus-visible:ring-offset-0"
+                        placeholder="Enter server name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="px-6 py-4 bg-gray-100">
+              <Button variant="primary" disabled={isLoading}>
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default InitialModal;

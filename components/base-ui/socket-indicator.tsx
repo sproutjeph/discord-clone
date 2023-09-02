9ff7a8eeaeb28843c8ec9331723823b06a41cceb
jephthah <jephthah.mbah@outlook.com>
2023-09-02 14:02:33 +0100

    socket io

--- components/base-ui/socket-indicator.tsx
+++ components/base-ui/socket-indicator.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-// import { useSocket } from "@/components/providers/socket-provider";
+import { useSocket } from "@/components/providers/socket-provider";
 import { Badge } from "@/components/ui/badge";
 
 export const SocketIndicator = () => {
-  // const { isConnected } = useSocket();
+  const { isConnected } = useSocket();
 
-  if (false) {
+  if (!isConnected) {
     return (
       <Badge variant="outline" className="text-white bg-yellow-600 border-none">
         Fallback: Polling every 1s
